c3e6adafa677d23f99ab5f512bddffd2ebee6b0e
NicoHaxz <Darwingral@gmail.com>
2025-10-25 06:40:56 -0500

    test

--- src/main/java/nico/dev/packetGlowAPI/API/IGlowAPI.java
+++ src/main/java/nico/dev/packetGlowAPI/API/IGlowAPI.java
@@ -1,4 +1,15 @@
 package nico.dev.packetGlowAPI.API;
 
+import nico.dev.packetGlowAPI.Colors.GlowColor;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+
+import java.util.Collection;
+import java.util.UUID;
+
 public interface IGlowAPI {
+    void setGlowing(Entity entity, Collection<Player> viewers, GlowColor color, boolean enable);
+    void setGlowingForAll(Entity entity, GlowColor color, boolean enable);
+    void setGlowingForPlayer(Entity entity, Player viewer, GlowColor color, boolean enable);
+    void removeEntityFromCache(UUID uuid);
 }
